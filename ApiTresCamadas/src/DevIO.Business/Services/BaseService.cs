1c998391abfb92d765976a6f20b63c97944773d0
Wesley-Silva <wesleybf3@gmail.com>
2025-04-28 09:40:31 -0300

    Implementando validações

--- ApiTresCamadas/src/DevIO.Business/Services/BaseService.cs
+++ ApiTresCamadas/src/DevIO.Business/Services/BaseService.cs
@@ -1,6 +1,25 @@
-﻿namespace DevIO.Business.Services
+﻿using DevIO.Business.Models;
+using DevIO.Business.Validation.Documentos.Dev.Business.Models.Base.Validations.Documentos;
+using FluentValidation;
+
+namespace DevIO.Business.Services
 {
     public abstract class BaseService
     {
+        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) 
+            where TV : AbstractValidator<TE>
+            where TE : Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            if (validator.IsValid)
+            {
+                return true;
+            }
+
+            // Lançamento de notificações
+
+            return false;
+        }
     }
 }
