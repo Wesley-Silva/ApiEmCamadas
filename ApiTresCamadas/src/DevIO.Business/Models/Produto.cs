6d13ab26e4f01b8fb2bc04d7eaf21eab0ea229cb
Wesley-Silva <wesleybf3@gmail.com>
2025-04-30 15:59:56 -0300

    Definindo contexto do EF, mapeado as entidades e customizando comportamentos do EF

--- ApiTresCamadas/src/DevIO.Business/Models/Produto.cs
+++ ApiTresCamadas/src/DevIO.Business/Models/Produto.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DevIO.Business.Models
+﻿namespace DevIO.Business.Models
 {
     public class Produto : Entity
     {
+        public Guid FornecedorId { get; set; }
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
         public decimal Valor { get; set; }
         public DateTime DataCadastro { get; set; }
         public bool Ativo { get; set; }
+
+        /* EF Relation */
+        public Fornecedor Fornecedor { get; set; }
     }
 }
