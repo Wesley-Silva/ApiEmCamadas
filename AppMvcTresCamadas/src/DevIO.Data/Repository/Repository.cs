26fd65f1e69b3191e15bf3599098d796441dec13
Wesley-Silva <wesleybf3@gmail.com>
2025-05-31 09:37:53 -0300

    Implementado repositorio generico e adicionado pacotes EF

---
+++ AppMvcTresCamadas/src/DevIO.Data/Repository/Repository.cs
@@ -0,0 +1,62 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DevIO.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new ()
+    {
+        protected readonly DbContext Db;
+        protected readonly DbSet<TEntity> DbSet;
+
+        protected Repository(DbContext db)
+        {
+            Db = db;
+            DbSet = db.Set<TEntity>();
+        }
+
+        public async Task<TEntity> ObterPorId(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<List<TEntity>> ObterTodos()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task Adicionar(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await SaveChange();
+        }
+
+        public virtual async Task Atualizar(TEntity entity)
+        {
+            DbSet.Update(entity);
+            await SaveChange();
+        }
+
+        public virtual async Task Remover(Guid id)
+        {
+            DbSet.Remove(new TEntity { Id = id});
+            await SaveChange();
+        }
+
+        public async Task<int> SaveChange()
+        {
+            return await Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Db.Dispose();
+        }
+    }
+}
