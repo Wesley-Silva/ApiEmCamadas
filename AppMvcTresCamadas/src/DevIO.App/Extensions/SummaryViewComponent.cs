583a02816a552cb8400100654c51dc1b4bf2178e
Wesley-Silva <wesleybf3@gmail.com>
2025-08-13 17:48:44 -0300

    Adicionado notificações baseado em viewcomponent

---
+++ AppMvcTresCamadas/src/DevIO.App/Extensions/SummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using DevIO.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.App.Extensions
+{
+    public class SummaryViewComponent : ViewComponent
+    {
+        private readonly INotificador _notificador;
+
+        public SummaryViewComponent(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var notificações = await Task.FromResult(_notificador.ObterNotificacoes());
+            notificações.ForEach(c => ViewData.ModelState.AddModelError(string.Empty, c.Mensagem));
+
+            return View();
+        }
+    }
+}
